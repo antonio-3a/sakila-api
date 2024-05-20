e83011775fd66cdf70beabe47831ff1792ae298b
António António <antonioandroid3a@gmail.com>
2024-05-20 17:08:31 +0100

    initial spring boot application setup

---
+++ src/main/java/dev/antonio3a/sakilaapi/SakilaApiApplication.java
@@ -0,0 +1,13 @@
+package dev.antonio3a.sakilaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SakilaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SakilaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/antonio3a/sakilaapi/SakilaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.antonio3a.sakilaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SakilaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
